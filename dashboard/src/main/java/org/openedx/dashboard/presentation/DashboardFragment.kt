d57c850ae69bae768b60d9527facf99d504c78a2
Saksham Agrawal <saksham@opencraft.com>
2024-09-24 20:10:29 +0530

    feat: Removing dropLast function from the URL to fix the course thumbnail

--- dashboard/src/main/java/org/openedx/dashboard/presentation/DashboardFragment.kt
+++ dashboard/src/main/java/org/openedx/dashboard/presentation/DashboardFragment.kt
@@ -388,7 +388,8 @@ private fun CourseItem(
             )
         )
     }
-    val imageUrl = apiHostUrl.dropLast(1) + enrolledCourse.course.courseImage
+    val imageUrl = apiHostUrl + enrolledCourse.course.courseImage
+
     val context = LocalContext.current
     Surface(
         modifier = Modifier

f782d7258f42a2d880f0a0a0f0f2b7260c396482
Saksham Agrawal <40560369+saksham115@users.noreply.github.com>
2024-09-27 00:54:10 +0530

    feat: Adding the course number and course effort on the course cards
    Co-authored-by: Paulo Viadanna <paulo@opencraft.com>
    (cherry picked from commit fc84b6362de317307677dcc089bbff4a99c53f4e)

--- dashboard/src/main/java/org/openedx/dashboard/domain/interactor/DashboardInteractor.kt
+++ dashboard/src/main/java/org/openedx/dashboard/domain/interactor/DashboardInteractor.kt
@@ -2,14 +2,25 @@ package org.openedx.dashboard.domain.interactor
 
 import org.openedx.core.domain.model.DashboardCourseList
 import org.openedx.dashboard.data.repository.DashboardRepository
+import org.openedx.discovery.domain.model.Course
+import org.openedx.discovery.data.repository.DiscoveryRepository
 
 class DashboardInteractor(
-    private val repository: DashboardRepository
+    private val dashboardRepository: DashboardRepository,
+    private val discoveryRepository: DiscoveryRepository
 ) {
 
     suspend fun getEnrolledCourses(page: Int): DashboardCourseList {
-        return repository.getEnrolledCourses(page)
+        return dashboardRepository.getEnrolledCourses(page)
     }
 
-    suspend fun getEnrolledCoursesFromCache() = repository.getEnrolledCoursesFromCache()
+    suspend fun getEnrolledCoursesFromCache() = dashboardRepository.getEnrolledCoursesFromCache()
+
+    suspend fun getCourseDetails(courseId: String): Course {
+        return discoveryRepository.getCourseDetail(courseId)
+    }
+
+    suspend fun getCourseDetailsFromCache(courseId: String): Course? {
+        return discoveryRepository.getCourseDetailFromCache(courseId)
+    }
 }
